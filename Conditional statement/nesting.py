7f01ab03400253fe513377cc4bcaf64183f42ea3
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-10 23:59:19 +0530

    Create nesting.py

---
+++ Conditional statement/nesting.py
@@ -0,0 +1,13 @@
+age = 95
+
+#nesting
+if(age >= 18):
+    if(age >= 80):
+        print("cannot drive")
+    else:
+        print("can drive")
+else:
+    print("cannot drive")
+
+#Output:
+cannot drive
