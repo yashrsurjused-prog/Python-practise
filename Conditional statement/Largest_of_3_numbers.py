454d41a1bdd85763fc3fe20d8971555c1051574f
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-11 00:12:27 +0530

    Create Largest_of_3_numbers.py

---
+++ Conditional statement/Largest_of_3_numbers.py
@@ -0,0 +1,17 @@
+a = int(input("Enter First number : "))
+b = int(input("Enter Second number : "))
+c = int(input("Enter Third number : "))
+
+if(a >= b and a >= c):
+    print("First number is largest", a)
+elif(b >=c ):
+    print("Second number is largest", b)
+else:
+    print("Third number is largest", c)
+
+
+#output:
+Enter First number : 90 
+Enter Second number : 67
+Enter Third number : 43
+First number is largest 90
