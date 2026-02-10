8d954379a9d31737b60543b827984ad530c31d84
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-11 00:03:25 +0530

    Create Even_or_odd.py

---
+++ Conditional statement/Even_or_odd.py
@@ -0,0 +1,15 @@
+number = int(input("Enter number: "))
+
+remainder = number % 2 
+
+if(remainder == 0):
+    print("EVEN")
+else:
+    print("Odd")
+
+#output:
+Enter number: 90
+EVEN
+
+Enter number: 87
+Odd
