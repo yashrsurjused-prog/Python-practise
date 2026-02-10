7994891ffe0276ec899a8bbe73d084537c221e40
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-10 22:18:36 +0530

    Update capitalize.py

--- String/String function/capitalize.py
+++ String/String function/capitalize.py
@@ -3,3 +3,13 @@ print(str.capitalize())
 
 #output:
 Yash surjuse is very good and finest man.
+
+
+str = "yash Surjuse is very good and finest man."
+str = str.capitalize()
+print(str)
+print(str)
+
+#output:
+Yash surjuse is very good and finest man.
+Yash surjuse is very good and finest man.
