9337a08e53877d843c1a4d40de7d2b1149372096
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-10 22:10:19 +0530

    Create endswith.py

---
+++ String/String function/endswith.py
@@ -0,0 +1,10 @@
+str = "Yash Surjuse is very good and finest"
+print(str.endswith("est"))
+
+#output:
+True
+
+str = "Yash Surjuse is very good and finest"
+print(str.endswith("best"))
+#output:
+False
