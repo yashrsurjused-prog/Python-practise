2e0048b8b14dc5e5483d7c007280aae85f84c4b7
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-10 23:06:53 +0530

    Create Q1).py

---
+++ String/Let's practise (L-2)/Q1).py
@@ -0,0 +1,8 @@
+1) write a program to import users, first name and print its length?
+
+name = input("Enter your name : ")
+print("Length of your name is", len(name))
+
+#output:
+Enter your name : Yash Surjuse
+Length of your name is 12
