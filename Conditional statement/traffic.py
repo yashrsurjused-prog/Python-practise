5dbbd193072bd2f860fcb333ff0b4cd491123354
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-10 23:41:42 +0530

    Update traffic.py

--- Conditional statement/traffic.py
+++ Conditional statement/traffic.py
@@ -9,3 +9,18 @@ elif(light == "yellow"):
 
 #output:
 go
+
+
+light = "pink"
+
+if(light == "red"):
+    print("Stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("Light is broken")
+
+#Output:
+Light is broken
