c0a57405435fdf1a5e9db1ea4946e566cd589b12
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-11 08:34:51 +0530

    Create Q1].py

---
+++ Lists/Q1].py
@@ -0,0 +1,13 @@
+marks = [94.4, 80.9, 98.9, 76.9, 30.2]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[1])
+
+#output:
+[94.4, 80.9, 98.9, 76.9, 30.2]
+<class 'list'>
+5
+94.4
+80.9
