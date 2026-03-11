a368e595ba7a9e962a675ea2335f49aa351c1876
Yash Surjuse <yashrsurjused@gmail.com>
2026-03-11 11:28:59 +0530

    Create myDict.keys().py

---
+++ Dictionary/Dictionary_methods/myDict.keys().py
@@ -0,0 +1,13 @@
+student = {
+    "name": "rahul kumar",
+    "subjects": {
+        "phy": 97,
+        "chem": 98,
+        "math": 95
+    }
+}
+
+print(student.keys())
+
+#Your Output
+dict_keys(['name', 'subjects'])
